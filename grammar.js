150b7377690909bafa991cf740cdfaca932d5f2e
atchman <atchman@users.noreply.github.com>
2025-04-24 22:24:33 +0200

    feat: comment rule first try

--- grammar.js
+++ grammar.js
@@ -10,8 +10,25 @@
 module.exports = grammar({
   name: "quadlet",
 
+  extra: $ => [
+    $.comment,
+  ],
+
   rules: {
     // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => seq(
+      optional($.new_blank),
+      repeat($.comment),
+      optional($.new_blank),
+    )
+    
+    new_blank: $ => optional(
+      /\n,
+    )
+
+    comment: $ => seq(
+      repeat1(";"),
+      repeat1("#"),
+    )
   }
 });
